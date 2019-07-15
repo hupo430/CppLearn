6520e03519baf8597c745039e9cbc1097b6441a7
breathhu <breathhu@lexinfintech.com>
2019-07-15 16:30:51 +0800

    创建开发分支

--- c_Class.cpp
+++ c_Class.cpp
@@ -1,38 +1,26 @@
-#include <iostream>
-#include <string>
+#include "c_Class.h"
 
-using namespace std;
 
-namespace breath{
 
-	class root{
-	private:
-		int aa;
 
-	public:
-		string bb;
-		void setaa(int a){};
-		int getaa(){};
-		
-	};	
-	
-	
-}
-
-void breath::root:setaa(int a){
+void breath::root::setaa(int a){
 	aa = a;
-	count<<"root set aa = "<<a<<endl;
-}
+	cout<<"root set aa = "<<a<<endl;
+	}
 
-int breath::root:getaa(){
-	
-	cout<<"root get aa = "<<aa<<endl;
-	return aa;         
-}
+int breath::root::getaa(){
 
+cout<<"root get aa = "<<aa<<endl;
+return aa;         
+}
 
 int main(){
 	
 	cout<<"end main"<<endl;
+	breath::root *rt = new breath::root();
+    cout<<rt->getaa()<<endl;
+    rt->setaa(3);
+    cout<<rt->getaa()<<endl;
+	delete rt;
 	return 0;
 }
---
+++ c_Class.h
@@ -0,0 +1,27 @@
+#ifndef _C_CLASS_H
+#define _C_CLASS_H
+
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+
+
+namespace breath{
+
+	class root{
+	private:
+		int aa;
+
+	public:
+		string bb;
+		void setaa(int a);
+		int getaa();
+		
+	};	
+	
+	
+}
+
+#endif
